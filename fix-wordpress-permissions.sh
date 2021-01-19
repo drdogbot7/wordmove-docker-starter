ff062f1597ab6f845400a0ba25dd3ca657772de6
Jeremy Mullis <jeremy.mullis@gmail.com>
2021-01-19 16:56:17 -0600

    more efficient commands

--- fix-wordpress-permissions.sh
+++ fix-wordpress-permissions.sh
@@ -9,10 +9,10 @@ WS_GROUP=www-data # <-- webserver group
 echo "Fixing Wordpress Permissions…"
 
 echo "Setting group to ${WS_GROUP}"
-find wp-content -exec chgrp ${WS_GROUP} {} \;
+chgrp -R ${WS_GROUP} wp-content
 
 echo "Setting directory permissions to 775"
-find wp-content -type d -not -path '*/\.git/*' -exec chmod 775 {} \;
+find wp-content -type d ! -path '*/\.git/*' ! -path '*/\node_modules/*' -exec chmod 775 {} \;
 
 echo "Setting file permissions to 664"
-find wp-content -type f -not -path '*/\.git/*' -exec chmod 664 {} \;
+find wp-content -type f ! -path '*/\.git/*' ! -path '*/\node_modules/*' -exec chmod 664 {} \;
